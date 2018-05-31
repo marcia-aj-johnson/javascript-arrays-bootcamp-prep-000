3a2a623132316e233f044829bdacd8c0f9bf86d4
Marcia Johnson <marcia.johnson87@gmail.com>
2018-05-31 14:03:50 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -29,8 +29,8 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
   }
     
   function removeElementFromBeginningOfArray (array) {
-    array.slice(1)
-    return array
+    var newArray = array.slice(1)
+    return newArray
   }
     
   function destructivelyRemoveElementFromEndOfArray (array) {
